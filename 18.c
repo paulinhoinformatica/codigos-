84bebcd46032c6a7202b0a1eec47d162e16ace31
Paulo <p69951175@gmail.com>
2026-05-22 21:42:36 -0300

    ai o codigo

---
+++ 18.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int M[3][3], V[3], soma = 0, a=0;
+    printf("Digite seus valores: ");
+    printf("\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &M[i][j]);
+        }
+        
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        V[i] = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            V[i] = V[i] + M[i][j];
+        }
+    }
+    
+    for (int i = 0; i < 3; i++)
+    {
+        printf("V%d: %d\n", i+1, V[i]);
+    }
+    
+}
